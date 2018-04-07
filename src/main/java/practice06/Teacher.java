3c56a0376c4754b1ea4479c1f56ec639b62c0f8f
Jokerblazes <zcd123@qq.com>
2018-04-07 13:20:21 +0800

    完成practice06的功能和测试

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private static final int NO_CLASS = -1;
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        klass = NO_CLASS;
+    }
+
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == NO_CLASS)
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        else
+            return super.introduce() + " I am a Teacher. I teach Class " + klass + ".";
+    }
 }
