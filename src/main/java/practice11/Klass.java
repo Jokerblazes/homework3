b31afdfd67158a3f081cae658d2fcd8d1dbe3ba3
Jokerblazes <zcd123@qq.com>
2018-04-07 19:41:25 +0800

    完成Practice11功能和测试

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,55 @@
 package practice11;
 
-public class Klass {
+import java.util.Observable;
+
+public class Klass extends Observable {
+    private final int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (super.equals(obj))
+            return true;
+        if (!(obj instanceof Klass))
+            return false;
+        return number == ((Klass) obj).getNumber();
+    }
+
+    public void assignLeader(Student student) {
+        if (this.equals(student.getKlass())) {
+            this.leader = student;
+            this.setChanged();
+            notifyObservers(student.getName() + " become Leader of Class " + this.number);
+        } else {
+            System.out.println("It is not one of us.");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        this.setChanged();
+        notifyObservers(student.getName() + " has joined Class " + this.number);
+    }
+
+
+    public boolean isIn(Student student) {
+        return this.equals(student.getKlass());
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,71 @@
 package practice11;
 
-public class Teacher {
+import java.util.HashSet;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Set;
+
+public class Teacher extends Person implements Observer {
+
+    private Set<Klass> classes;
+
+    public Teacher(int id, String name, int age, Set<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        classes.forEach(klass -> klass.addObserver(this));
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Set<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        if (classes == null)
+            return basicIntroduce() + " I teach No Class.";
+        else {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(basicIntroduce())
+                    .append(" I teach Class");
+            //由于HashSet并不会根据大小排序，所以先取出数值排序，再返回
+            Set<Integer> set = new HashSet<>();
+            classes.forEach(klass -> set.add(klass.getNumber()));
+            set.stream().sorted().forEach(integer -> stringBuilder.append(" ")
+                    .append(integer).
+                            append(","));
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1)
+                    .append(".");
+            return stringBuilder.toString();
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (isTeaching(student)) {
+            return basicIntroduce() +  " I teach " + student.getName() + ".";
+        } else
+            return basicIntroduce() +  " I don't teach " + student.getName() + ".";
+    }
+
+    private String basicIntroduce() {
+        return super.introduce() + " I am a Teacher.";
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass klass : classes) {
+            if (klass.isIn(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("I am " + getName() + ". I know " + arg + ".");
+    }
 }
+
