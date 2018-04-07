025a1738595d57303cd709a1e651ef687882c1c9
Jokerblazes <zcd123@qq.com>
2018-04-07 13:52:15 +0800

    完成practice07的功能和测试

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,27 @@
 package practice07;
 
 public class Klass {
+    private final int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (super.equals(obj))
+            return true;
+        if (!(obj instanceof Klass))
+            return false;
+        return number == ((Klass) obj).getNumber();
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
+
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,39 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null)
+            return basicIntroduce() + " I teach No Class.";
+        else
+            return basicIntroduce() +  " I teach " + klass.getDisplayName() + ".";
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().equals(klass)) {
+            return basicIntroduce() +  " I teach " + student.getName() + ".";
+        } else
+            return basicIntroduce() +  " I don't teach " + student.getName() + ".";
+    }
+
+    private String basicIntroduce() {
+        return super.introduce() + " I am a Teacher.";
+    }
 }
