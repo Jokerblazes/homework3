81c0a2b7f5213b1ea5c017ddc6cda8bcadddbf45
Jokerblazes <zcd123@qq.com>
2018-04-07 14:16:18 +0800

    完成practice08的功能和测试

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,38 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null)
+            return basicIntroduce() + " I teach No Class.";
+        else
+            return basicIntroduce() +  " I teach " + klass.getDisplayName() + ".";
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().equals(klass)) {
+            return basicIntroduce() +  " I teach " + student.getName() + ".";
+        } else
+            return basicIntroduce() +  " I don't teach " + student.getName() + ".";
+    }
+
+    private String basicIntroduce() {
+        return super.introduce() + " I am a Teacher.";
+    }
 }
